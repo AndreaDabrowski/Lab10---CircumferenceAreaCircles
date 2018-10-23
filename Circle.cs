36d697715813dfeab62b4e19cf8688070e4029e7
AndreaDabrowski <AnnMDabrowski@gmail.com>
2018-10-22 21:21:04 -0400

    lab 10 stuff

---
+++ Circle.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10
+{
+    class Circle
+    {
+        private double radius;
+        public const double PI = System.Math.PI;//field
+        public double Radius//property, not used in constructor because ????
+        {
+            get
+            {
+                return radius;
+            }
+            set
+            {
+                radius = value;
+            }
+        }
+        public Circle(double radius)
+        {
+            this.radius = radius;//initializing the variable
+        }
+        public double CalculateCircumference()
+        {
+            double circumference = PI * (2 * radius);
+            return circumference;
+            //allow for user input on number of decimal places
+        }
+        public string CalculateFormattedCircumference()
+        {
+
+            return FormatNumber(CalculateCircumference());
+        }
+        public double CalculateArea()
+        {
+            
+            double area = PI * radius * radius;
+            return area;
+        }
+        public string CalculateFormattedArea()
+        {
+            string formattedArea = FormatNumber(CalculateArea());
+            return formattedArea;
+
+        }
+        private string FormatNumber(double x)
+        {
+            return $"{x,0:N2}";//$ signals incantination, then index of zero and going two decimal places
+        }
+
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string response;
+            int counter = 1;
+            while (true) 
+            {
+                Console.WriteLine("LETS FIND THE CIRCUMFERENCE AND AREA OF A CIRCLE!");
+                double radius = ValidateRadius();
+
+                Circle myCircle = new Circle(radius);//sets radius to user input
+
+                Console.WriteLine($"Circumference: {myCircle.CalculateCircumference()}"); //user mycircle called circumference method
+                Console.WriteLine($"Circumference formatted: {myCircle.CalculateFormattedCircumference()}");//called formatted circumference method
+
+                Console.WriteLine($"Area: {myCircle.CalculateArea()}");//calculates area
+                Console.WriteLine($"Area Formatted: {myCircle.CalculateFormattedArea()}");//calculates formatted area
+
+                Console.WriteLine("Would you like to continue? (y/n)");
+                response = Console.ReadLine().ToLower();
+                if (YesOrNo(response) == true)
+                {
+                    counter++;
+                }
+                else
+                {
+                    break;
+                }
+
+
+            }
+            Console.WriteLine("GoodBye! You created {0} Circles!", counter); //, VALUE);
+            Console.ReadLine();
+            
+        }
+        //verifies correct format for y or n responses
+        public static bool YesOrNo(string response)
+        {
+
+            while (true)
+            {
+
+                if (response == "y")
+                {
+                    return true;
+                }
+                else if (response == "n")
+                {
+                    return false;
+                }
+                else
+                {
+                    Console.Write("Invalid input, try (y/n): ");
+                    response = Console.ReadLine().ToLower();
+
+                }
+            }
+
+        }
+        public static double ValidateRadius()
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Please enter the circle's radius: ");
+                    double num = double.Parse(Console.ReadLine());
+                    return num;
+
+                }
+                catch (FormatException)
+                {
+                    Console.Write("Invalid Format - Input valid number: ");
+                }
+            }
+        }
+    }
+}
